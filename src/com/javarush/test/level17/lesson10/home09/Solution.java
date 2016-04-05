6cee1fea5e2cc94a9be1f721449c50f491e88a85
Sergei <Faoxis@mail.ru>
2016-04-05 07:57:23 +0300

    level 17

---
+++ src/com/javarush/test/level17/lesson10/home09/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.test.level17.lesson10.home09;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* Транзакционность
+Сделать метод joinData транзакционным, т.е. если произошел сбой, то данные не должны быть изменены.
+1. Считать с консоли 2 имени файла
+2. Считать построчно данные из файлов. Из первого файла - в allLines, из второго - в forRemoveLines
+В методе joinData:
+3. Если список allLines содержит все строки из forRemoveLines, то удалить из списка allLines все строки, которые есть в forRemoveLines
+4. Если список allLines НЕ содержит каких-либо строк, которые есть в forRemoveLines, то
+4.1. очистить allLines от данных
+4.2. выбросить исключение CorruptedDataException
+Метод joinData должен вызываться в main. Все исключения обработайте в методе main.
+*/
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader file1 = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedReader file2 = new BufferedReader(new FileReader(reader.readLine()));
+
+        String line;
+
+        while ( (line = file1.readLine()) != null) {
+            allLines.add(line);
+        }
+
+        while ( (line = file2.readLine()) != null) {
+            forRemoveLines.add(line);
+        }
+
+    }
+
+    public synchronized void joinData () throws CorruptedDataException {
+        boolean thereIs = true;
+
+        for (int i = 0; i < allLines.size(); i++) {
+            for (int j = 0; j < forRemoveLines.size(); j++) {
+                if (allLines.get(i).equals(forRemoveLines.get(j))) {
+
+
+                }
+            }
+        }
+
+    }
+}
