470ecea8d43481f32c07eeaa6b7d2846df1248b6
Sergei <Faoxis@mail.ru>
2016-07-24 17:30:57 +0300

    just a commit

---
+++ src/com/javarush/test/level18/lesson10/home07/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level18.lesson10.home07;
+
+/* Поиск данных внутри файла
+Считать с консоли имя файла
+Найти в файле информацию, которая относится к заданному id, и вывести ее на экран в виде, в котором она записана в файле.
+Программа запускается с одним параметром: id (int)
+Закрыть потоки. Не использовать try-with-resources
+
+В файле данные разделены пробелом и хранятся в следующей последовательности:
+id productName price quantity
+
+где id - int
+productName - название товара, может содержать пробелы, String
+price - цена, double
+quantity - количество, int
+
+Информация по каждому товару хранится в отдельной строке
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+//./JavaRush/src/com/javarush/test/level18/lesson10/home07
+        BufferedReader fileReader = new BufferedReader(new InputStreamReader(
+                new FileInputStream("/home/sergei/test.txt")));
+
+
+    }
+}
