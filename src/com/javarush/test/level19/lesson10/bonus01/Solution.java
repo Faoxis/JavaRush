7e7fe372c0288399a13668a883703cfba183542c
Sergei <Faoxis@mail.ru>
2016-10-04 21:23:03 +0300

    level 20 has been started

--- src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level19.lesson10.bonus01;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +33,15 @@ file1:         file2:             результат:(lines)
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader file1 = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedReader file2 = new BufferedReader(new FileReader(reader.readLine()));
+
+
+        reader.close();
+        file1.close();
+        file2.close();
     }
 
 
--- src/com/javarush/test/level19/lesson10/bonus02/TestFileConsoleWriter.java
+++ src/com/javarush/test/level19/lesson10/bonus02/TestFileConsoleWriter.java
@@ -1,7 +1,21 @@
 package com.javarush.test.level19.lesson10.bonus02;
 
+import java.io.IOException;
+
 /**
  * Created by sergei on 10/4/16.
  */
 public class TestFileConsoleWriter {
+    public static void main(String[] args) throws IOException {
+        FileConsoleWriter fileConsoleWriter = new FileConsoleWriter("/home/sergei/file1.txt");
+        char[] mass = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        char c = '2';
+        fileConsoleWriter.write("Hello there!");
+        fileConsoleWriter.write(mass);
+        fileConsoleWriter.write('9');
+        fileConsoleWriter.write(mass, 2, 5);
+        fileConsoleWriter.write("Hello there", 2, 5);
+
+        fileConsoleWriter.close();
+    }
 }
