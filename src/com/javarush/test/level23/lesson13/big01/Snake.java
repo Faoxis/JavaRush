5e136781baf5bc65ca1c0e0769e6c0bf1251ac24
Сергей Самойлов <samojlov_sg@astralnalog.ru>
2016-12-05 17:27:38 +0300

    23.13

---
+++ src/com/javarush/test/level23/lesson13/big01/Snake.java
@@ -0,0 +1,58 @@
+package com.javarush.test.level23.lesson13.big01;
+
+import java.util.ArrayList;
+
+
+public class Snake {
+    private ArrayList<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public void move() {
+
+    }
+
+    public ArrayList<SnakeSection> getSections()
+    {
+        return sections;
+    }
+
+    public boolean isAlive()
+    {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection()
+    {
+        return direction;
+    }
+
+    public void setSections(ArrayList<SnakeSection> sections)
+    {
+        this.sections = sections;
+    }
+
+    public void setAlive(boolean alive)
+    {
+        isAlive = alive;
+    }
+
+    public void setDirection(SnakeDirection direction)
+    {
+        this.direction = direction;
+    }
+}
