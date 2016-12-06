c65f7cdf454c7dd88a049ba743b3f21f163b8cf9
Сергей Самойлов <samojlov_sg@astralnalog.ru>
2016-12-06 12:25:35 +0300

    24.02

---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by samojlov on 06.12.16.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void hello() {
+        System.out.println("Hello!");
+    }
+
+    public void hi() {
+        System.out.println("Hi!");
+    }
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/UnsupportedInterfaceMarkerException.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by samojlov on 06.12.16.
+ */
+public class UnsupportedInterfaceMarkerException extends Exception {
+
+}
