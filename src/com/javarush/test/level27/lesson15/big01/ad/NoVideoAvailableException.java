dd7d05f0d1d4753ca6b777458905201e2c7b2f4d
Сергей Самойлов <samojlov_sg@astralnalog.ru>
2017-01-12 17:04:46 +0300

    27.15 9 It is exactly not success

--- src/com/javarush/test/level27/lesson15/big01/ad/NoVideoAvailableException.java
+++ src/com/javarush/test/level27/lesson15/big01/ad/NoVideoAvailableException.java
@@ -3,5 +3,5 @@ package com.javarush.test.level27.lesson15.big01.ad;
 /**
  * Created by sergei on 1/9/17.
  */
-public class NoVideoAvailableException {
+public class NoVideoAvailableException extends RuntimeException {
 }
